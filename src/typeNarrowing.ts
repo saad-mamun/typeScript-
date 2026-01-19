905cc439ae953aff00c300b11964bb58a8a50ae6
saad-mamun <mamunali88991@gmail.com>
2026-01-19 18:02:52 +0600

    typeNarrowing

---
+++ src/typeNarrowing.ts
@@ -0,0 +1,85 @@
+function getChai(kind: string | number) {
+  if (typeof kind === "string") {
+    return `Making ${kind} cah`;
+  }
+  return `Chai order ${kind}`;
+}
+
+function serveChai(msg?: string) {
+  if (msg) {
+    return `Serving ${msg}`;
+  }
+  return `Serving default masala chai`;
+}
+
+function orderChai(size: "small" | "medium" | "large" | number) {
+  if (size === "small") {
+    return `small cutting chai`;
+  } else if (size === "medium" || size === "large") {
+    return `make extra chai`;
+  }
+  return `chai order ${size}`;
+}
+
+class KulhadChai {
+  serve() {
+    return `Serving kulhad chai`;
+  }
+}
+class Cutting {
+  serve() {
+    return `Serving cutting chai`;
+  }
+}
+function serve(chai: KulhadChai | Cutting) {
+  if (chai instanceof KulhadChai) {
+    return chai.serve();
+  }
+}
+
+type ChaiOrder = {
+  type: string;
+  sugar: number;
+};
+function isChaiOrder(obj: any): obj is ChaiOrder {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    typeof obj.type === "string" &&
+    typeof obj.sugar === "number"
+  );
+}
+function serveOrder(item: ChaiOrder | string) {
+  if (isChaiOrder(item)) {
+    return `Serving ${item.type} chai with ${item.sugar}`;
+  }
+  return `Serving custom chai: ${item}`;
+}
+
+type MasalaChai = { type: "masala"; spicelevel: number };
+type GingerChai = { type: "ginger"; amount: number };
+type ElaichiChai = { type: "elaichichai"; aroma: number };
+
+type Chai = MasalaChai | GingerChai | ElaichiChai;
+function MakeChia(order: Chai) {
+  switch (order.type) {
+    case "masala":
+      return `Masala chai`;
+      break;
+    case "ginger":
+      return `Ginger chai`;
+      break;
+    case "elaichichai":
+      return `Elaichi CHAI`;
+      break;
+  }
+}
+
+function brew(order: MasalaChai | GingerChai){
+    if('spicelevel' in order){
+        //
+    }
+}
+
+// function isStingArray(arr: unknown): arr is string[]{
+// }
