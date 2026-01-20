3b9c877262d414662ce6bbcd9798ebda150a8c02
saad-mamun <mamunali88991@gmail.com>
2026-01-20 22:54:05 +0600

    InterfaceTs

---
+++ src/interfaceTs.ts
@@ -0,0 +1,74 @@
+
+type ChaiOrder = { 
+    type: string; 
+    sugar: number;
+    strong: boolean;
+    }
+
+function makeChai(order: ChaiOrder ) {
+    console.log(order)
+}
+function serveChai(order: ChaiOrder ) {
+    console.log(order)
+}
+
+// Jokhon number string boolean likha lage tokhon type use kora lage;
+type TeaRecipe = {
+    water: number;
+    milk: number;
+};
+class MasalaChai implements TeaRecipe{
+    water: number = 100;
+    milk: number = 50;
+}
+
+
+
+// hark code likhar somoy interface use korte hoy jamon "RandomText", 300, true asob;
+interface CupSize {
+  size: "Small Cup" | "Large Cup";
+} 
+class Chai implements CupSize{
+    size: "Small Cup" | "Large Cup" = "Large Cup";
+};
+
+
+//  | sign dia bujshi je kono akta hobe. ara OR  er moto kaj kore
+// TeaType line ta k litarel type bola hoy..
+type TeaType = "Masala" | 'ginger'| 'lemon';
+function orderChai(t: TeaType){
+    console.log(t)
+}
+
+
+
+type BaseChai = {teaLeaves: number};
+type Extra = {masala: number};
+// & sign dia bujai BaseChai and Extra dutoi akshathe  use korte hobe.
+type MasalaChaiTow = BaseChai & Extra;
+const cup: MasalaChaiTow = {
+    teaLeaves: 2,
+    masala:1
+}
+
+
+
+type User = {
+    username: string;
+    bio?: string;
+
+}
+const u1 : User = {username: "Liton"}
+const u2 : User = {username: "Liton", bio: "liton181.com"};
+
+
+type Config = {
+    readonly appName : string;
+    age: number;
+}
+const cfg: Config = {
+    appName : "LitonAlMamun",
+    age: 21,
+}
+//  readonly use korar karone appName akbar usekora jabe.
+// cfg.appName = "MILON"
---
+++ src/moreTypes.ts
@@ -0,0 +1,64 @@
+let response: any = "40";
+let numericLength: number = (response as string).length;
+
+type Book = {
+  name: string;
+};
+let bookString = '{"name" : "Who moved my cheese"}';
+let bookObject = JSON.parse(bookString) as Book;
+console.log(BookObject);
+const inputElement = document.getElementById("username") as HTMLInputElement;
+
+let value: any;
+value = "CHAI";
+value = [1, 2, 3, 4];
+value = 1,2,3;
+value.toUpperCase();
+
+let newValue: unknown;
+newValue = 'Chai';
+newValue = [1,2,3];
+newValue = 1,2,3;
+// newValue.toUpperCase() ATA WRONG
+if(typeof newValue === "string"){
+    newValue.toUpperCase();
+}
+
+
+
+
+// try & catch
+try{
+
+} catch (error) {
+    if(error instanceof Error){
+        console.log(error.message)
+    }
+    console.log("Error", error)
+}
+
+
+const data: unknown = "Liton al mamun";
+// ata wrong
+// const strData: string = data 
+// Force kore string(as string) kora hoyse
+const strData: string = data as string
+
+
+type Role = "admin" | "user";
+
+function redirectBasedOnRole(role: Role): void {
+    if(role === "admin"){
+        console.log('Redirecting to admin dashboard');
+        return
+    } else if(role === 'user'){
+         console.log("Redirecting to User dashboard");
+         return;
+    }
+    role;
+} 
+
+
+function neverReturn():never{
+    while(true){}
+}
