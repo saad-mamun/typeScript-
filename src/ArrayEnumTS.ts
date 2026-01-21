6a49549c8201530d6a86b624ccb5e46c4aa81e36
saad-mamun <mamunali88991@gmail.com>
2026-01-21 23:48:34 +0600

    ArrayEnumTuples

---
+++ src/ArrayEnumTS.ts
@@ -0,0 +1,62 @@
+const chaiFlavours: string[] = ["Masala", "Adrak"];
+const chaiPrice: number[] = [10, 20];
+// nicher taw array
+const rating: Array<number> = [4.5, 6.3, 4];
+
+type Chai = {
+  name: string;
+  price: number;
+};
+const menu: Chai[] = [
+  { name: "Masala", price: 15 },
+  { name: "Ada", price: 25 },
+];
+
+const cities: readonly string[] = ["Dhaka", "Rajshahi"];
+// cities.push("D");
+
+const table: number[][] = [
+  [1, 2, 3],
+  [6, 5, 3],
+];
+
+// Tuples.............................
+let chaiType: [string, number, boolean];
+chaiType = ["lemon", 2, true];
+
+const ourReadonlyTuple: readonly [number, boolean, string] = [
+  5,
+  true,
+  "The Real Coding God",
+];
+const chaiItems : [name:string, price: number] = ['Masala', 23];
+
+
+
+// enum.....................................
+enum CupSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
+const size = CupSize.LARGE;
+
+// auto declear korbe
+enum Status {
+    PENDING = 100,
+    SERVED, // 101
+    CANCELED //102
+};
+
+
+enum ChaiType {
+    MASALA = "masala",
+    GINGER = "ginger"
+}
+function makeChai(type: ChaiType){
+    console.log(`Making: ${type}`);
+}
+makeChai(ChaiType.GINGER);
+
+let t: [string, number] = ["Chai", 10];
+t.push("extra")
