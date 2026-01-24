d3c8a51ab035367360b6d09cbfcf5c897005e12b
saad-mamun <mamunali88991@gmail.com>
2026-01-24 16:01:16 +0600

    InterfaceAndGen

---
+++ src/GenericsTs.ts
@@ -0,0 +1,33 @@
+ function wrapInArray<T>(item: T): T[]{
+    return [item]
+ }
+ wrapInArray("Masala");
+ wrapInArray(20);
+ wrapInArray({flavor: "Ginger"});
+
+
+ function pair<A, B>(a: A, b: B): [A, B] {
+    return [a, b]
+ }
+
+ pair('masala', 'lemon');
+ pair('masala', 20);
+ pair('masala', {flavor: "Ginger"});
+
+
+
+ interface Box<T>{
+    content: T
+ }
+ const numberBox: Box<number> = {content: 10}
+ const numberBoxCup: Box<string> = {content: '10'};
+
+ interface ApiPromise<T>{
+    status: number,
+    data: T
+ }
+
+ const res: ApiPromise<{flavor: string}> = {
+    status: 200,
+    data: {flavor: "Masala"}
+ }
---
+++ src/InterfaceT.ts
@@ -0,0 +1,67 @@
+interface Chai {
+  flavor: string;
+  price: number;
+  mild?: boolean;
+}
+const masala: Chai = {
+  flavor: "masala",
+  price: 10,
+};
+
+interface Shop {
+  readonly id: number;
+  name: string;
+}
+const s: Shop = { id: 1, name: "Codding off" };
+// s.id = 2; // Error: Cannot assign to 'id' because it is a read-only property.
+
+interface DicCountCalculator {
+  (price: number): number;
+}
+const apply50: DicCountCalculator = (p) => p * 0.5;
+
+interface TeaMachine {
+  start(): void;
+  stop(): void;
+}
+const machine: TeaMachine = {
+  start() {
+    console.log("Machine started");
+  },
+  stop() {
+    console.log("Machine stopped");
+  },
+};
+
+// Indexable (index signature) Types
+interface ChaiRating {
+  [flavor: string]: number;
+}
+const rating: ChaiRating = {
+  masala: 2.3,
+  ginger: 4.5,
+  lemon: 3.8,
+};
+
+// Marge hoy jai
+interface User {
+  name: string;
+}
+
+interface User {
+  age: number;
+}
+const u: User = {
+  name: "Liton",
+  age: 22,
+};
+
+interface A {
+  a: string;
+}
+interface B {
+  b: string;
+}
+interface C extends A, B {
+  c: string;
+}
