f8c3d54290f2a79a4f16f1afe004d9428857bb50
saad-mamun <mamunali2660@gmail.com>
2026-04-19 17:35:16 +0600

    in

--- src/typesInTs.ts
+++ src/typesInTs.ts
@@ -1,2 +1,8 @@
 let practice = "TypeScript";
-let cups = Math.random() > .5 ? 10 : 5
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+
+
+// For practice
+let DemoPractice = "TypeScript";
+let DemoCups = Math.random() > 0.5 ? 10 : 5;
