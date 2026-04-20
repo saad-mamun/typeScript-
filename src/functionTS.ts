c9ce0aecc08ef713c4e98859fd8be414bca9fb54
saad-mamun <mamunali2660@gmail.com>
2026-04-20 22:11:48 +0600

    function

--- src/functionTS.ts
+++ src/functionTS.ts
@@ -3,6 +3,12 @@ function makeChai(type: string, cup: number){
 }
 makeChai("Lemon", 4);
 
+
+function makeNewChai(type: string, cup: number){
+    console.log(`Making ${cup} cups of ${type}`)
+}
+makeNewChai("Lemon", 4);
+
 function newTea(type: string, price:number):string {
     return `Making ${type} with price ${price}`
 }
